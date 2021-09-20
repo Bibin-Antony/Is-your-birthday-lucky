534aa508005f1c71b905c6043571d691023de005
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2021-09-21 00:39:29 +0530

    phase 1 done
    logic part works well!

--- index.js
+++ index.js
@@ -13,22 +13,34 @@ function checkIfLucky(){
 }
 function calculateNumbers(birthDate){
     birthDate =  birthDate.replaceAll("-", "");
+    console.log(birthDate);
+    resultBox.style.display = "none";
+    if(birthDate > 0){
     let sum = 0;
     for(let index = 0; index <= birthDate.length; ++index){
         sum = sum + Number(birthDate.charAt(index));
     }
     console.log(sum, luckyNumber.value);
     sumNew = sum % Number(luckyNumber.value);
-     return sum, sumNew;   
+    return sum, sumNew;  
+    }
+    else{
+        showMessage("Please enter 👉 Date of Birth!")
+    }
+    
+      
      
 }
 
-
+function showMessage(message) {
+    resultBox.style.display = "block";
+    resultBox.innerHTML = message;
+}
 function birthdaycalc(sumNew){
     console.log(sumNew);
     if(sumNew == 0){
-        resultBox.innerText("Congragulations, Your bithday is lucky! 🎉🥳")
+        showMessage("Congragulations, Your bithday is lucky! 🎉🥳")
     }else{
-        resultBox.innerText("Sorry, Your Birthday isn't lucky! 😞")
+        showMessage("Sorry, Your Birthday isn't lucky! 😞")
     }
 }
