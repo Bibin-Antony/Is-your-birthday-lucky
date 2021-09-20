d9874c6daa6c078629981ad0d597b23e3245f730
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2021-09-20 21:26:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
 
-const DateOfBirth = Number(dateOfBirth.value);
+
+checkButton.addEventListener("click", checkIfLucky);
+
+function checkIfLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateNumbers(dob);
+    console.log(sum);
+    
+}
+function calculateNumbers(dob){
+    dob =  dob.replaceAll("-", "");
+    console.log(dob);
+    let sum = 0;
+    for(let index=0; index<dob.length; index++){
+        sum = sum + Number(dob.charAt(index));
+    }
+}
+
