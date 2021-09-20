b847c41ea71131139a64623089914a67c7b82693
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2021-09-21 00:07:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
-
-
+const resultBox = document.querySelector("#result-output-box");
+ 
 checkButton.addEventListener("click", checkIfLucky);
 
 function checkIfLucky(){
-    const dob = dateOfBirth.value;
-    const sum = calculateNumbers(dob);
-    console.log(sum);
+    const birthDate = dateOfBirth.value;
+    const  sum = calculateNumbers(birthDate);  
+    birthdaycalc(sumNew);
     
 }
-function calculateNumbers(dob){
-    dob =  dob.replaceAll("-", "");
-    console.log(dob);
+function calculateNumbers(birthDate){
+    birthDate =  birthDate.replaceAll("-", "");
     let sum = 0;
-    for(let index=0; index<dob.length; index++){
-        sum = sum + Number(dob.charAt(index));
+    for(let index = 0; index <= birthDate.length; ++index){
+        sum = sum + Number(birthDate.charAt(index));
     }
+    console.log(sum, luckyNumber.value);
+    sumNew = sum % Number(luckyNumber.value);
+     return sum, sumNew;   
+     
 }
 
+
+function birthdaycalc(sumNew){
+    console.log(sumNew);
+    if(sumNew == 0){
+        resultBox.innerText("Congragulations, Your bithday is lucky! 🎉🥳")
+    }else{
+        resultBox.innerText("Sorry, Your Birthday isn't lucky! 😞")
+    }
+}
