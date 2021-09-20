e7c83a9aaf9133b09d0ca2ec6b5285c74e80901e
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2021-09-20 18:03:07 +0530

    updated

---
+++ index.js
@@ -0,0 +1,5 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-button");
+
+const DateOfBirth = Number(dateOfBirth.value);
